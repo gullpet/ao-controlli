5e270d3606b28f54cf12f2384e07316b20994545
Giacomo Romanini <g.romanini2@gmail.com>
2021-07-01 10:20:58 +0200

    puliti i grafici

--- Prova.m
+++ Prova.m
@@ -34,7 +34,7 @@ s=tf('s');
 GG        = 1/( (dist_mas_iner) * ( s + ( (bb/(dist_mas_iner)) + (( (Ud*Mi*Ei*Ei)/(dist_mas_iner) )*2*We) ) ) );
 GG_Ei_min = 1/( (dist_mas_iner_min) * ( s + ( (bb/(dist_mas_iner_min)) + (( (Ud*Mi*Ei_min*Ei_min)/(dist_mas_iner_min) )*2*We) ) ) );
 GG_Ei_max = 1/( (dist_mas_iner_max) * ( s + ( (bb/(dist_mas_iner_max)) + (( (Ud*Mi*Ei_max*Ei_max)/(dist_mas_iner_max) )*2*We) ) ) );
-            
+
 
 figure(1)
 h_G2 = bodeplot(GG,{omega_range_min,omega_range_max});
@@ -57,23 +57,22 @@ omega_c_min = 460 / (Ta_1 * Mf);
 figure(2)
 [Mag,phase,omega] = bode(GG, {omega_range_min, omega_range_max}, 'k');
 
-patch([omega_range_min, omega_c_min, omega_c_min, omega_range_min], [0, 0, -200, -200], 'r', 'FaceAlpha', 0.3); grid on
-patch([omega_n, omega_range_max, omega_range_max, omega_n], [Bn, Bn, 200, 200], 'r', 'FaceAlpha', 0.3); grid on
+patch([omega_range_min, omega_c_min, omega_c_min, omega_range_min], [0, 0, -200, -200], 'r', 'FaceAlpha', 0.2); grid on
+patch([omega_n, omega_range_max, omega_range_max, omega_n], [Bn, Bn, 200, 200], 'r', 'FaceAlpha', 0.4); grid on
 
 
-Legend_dB=["B_n";
-    "\omega_{c,min}= 0.12"
-    "G(j\omega)" ];
+Legend_dB=["Attenuazione disturbo in uscita";
+    "Attenuazione disturbo di musura" ];
 legend(Legend_dB);
 
 hold on;
 margin(Mag,phase,omega);grid on;
 
 patch([omega_c_min, omega_n, omega_n, omega_c_min], [-200, -200, phase_L_jwc_min, phase_L_jwc_min], 'r', 'FaceAlpha', 0.3); grid on
-Legend_arg=["G_e(j\omega)";"M_{f,min}"] ;
+Legend_arg=["G(j\omega)";"M_{f,min}" ];
 legend(Legend_arg);
 
-
+title("Diagramma di bode del sistema linearizzato");
 hold off;
 
 %% Graphical constraints of the step response
@@ -82,7 +81,7 @@ W=5;
 S_100=0.01;
 T_simulation=15;
 
-% figure(52); 
+figure(3); 
 
 step(GG,T_simulation,'b')
 hold on
@@ -97,10 +96,11 @@ ylim([0,W+1]);
 patch([Ta_1,T_simulation,T_simulation,Ta_1],[W*(1+0.01),W*(1+0.01),W+1,W+1], 'green','FaceAlpha',0.2);
 
 patch([Ta_1,T_simulation,T_simulation,Ta_1],[W*(1-0.01),W*(1-0.01),0,0], 'green','FaceAlpha', 0.2);
-
-hold off
 Legend=["G(s) Step Response";"Overshoot Constraint"; "Settling time Cons"];
 legend(Legend);
+
+title("Risposta di G(s) a ingresso a gradino");
+hold off;
 
 
 %% Graphical constraints of the Ge
@@ -109,15 +109,15 @@ GGe        = (1/s) * GG;
 GGe_Ei_min = (1/s) * GG_Ei_min;
 GGe_Ei_max = (1/s) * GG_Ei_max;
 
-figure(126)
+figure(4)
+
 [Mag,phase,omega] = bode(GGe, {omega_range_min, omega_range_max}, 'k');
 
-patch([omega_range_min, omega_c_min, omega_c_min, omega_range_min], [0, 0, -200, -200], 'r', 'FaceAlpha', 0.3); grid on
-patch([omega_n, omega_range_max, omega_range_max, omega_n], [Bn, Bn, 200, 200], 'r', 'FaceAlpha', 0.3); grid on
+patch([omega_range_min, omega_c_min, omega_c_min, omega_range_min], [0, 0, -200, -200], 'r', 'FaceAlpha', 0.2); grid on
+patch([omega_n, omega_range_max, omega_range_max, omega_n], [Bn, Bn, 200, 200], 'r', 'FaceAlpha', 0.4); grid on
 
-Legend_dB=["B_n";
-    "\omega_{c,min}"
-    "G_e(j\omega)" ];
+Legend_dB=["Att. disturbo uscita";
+    "Att. disturbo misura" ];
 
 legend(Legend_dB);
 hold on;
@@ -126,15 +126,15 @@ margin(Mag,phase,omega);grid on;
 %non si vedrà, troppo in basso nel grafico
 patch([omega_c_min, omega_n, omega_n, omega_c_min], [-200, -200, phase_L_jwc_min, phase_L_jwc_min], 'r', 'FaceAlpha', 0.3); grid on 
 
-Legend_arg=["G_e(j\omega)";"M_{f,min}"] ;
+Legend_arg=["G_{e}(j\omega)";"M_{f,min}"] ;
 legend(Legend_arg);
-
+title("G(s) estesa");
 hold off;
 
 %% Rete Anticipatrice
 clc
 
-omega_c_star=omega_c_min+1.1;
+omega_c_star=omega_c_min+0.3;
 
 casual_n=( (bb/(dist_mas_iner)) + (( (Ud*Mi*Ei*Ei)/(dist_mas_iner) )*2*We) );
 GGe_omega_star = (1/(1i*omega_c_star) )*(1/( (dist_mas_iner) * ( (1i*omega_c_star) +  casual_n) ));
@@ -142,10 +142,10 @@ GGe_omega_star = (1/(1i*omega_c_star) )*(1/( (dist_mas_iner) * ( (1i*omega_c_sta
 ph=rad2deg(angle(GGe_omega_star));
 amplitude=mag2db(abs(GGe_omega_star));
 
-phi_star=(Mf+10)-180-ph;
+phi_star=(Mf+5)-180-ph;
 M_star=10.^(-amplitude/20);
 
-%{ control values
+
 disp("maggiore di 1");
 display(M_star);
 disp("tra 0 e pi/2");
@@ -153,50 +153,77 @@ display(phi_star);
 disp("sopra > sotto");
 display(cos(phi_star));
 display(1/M_star);
-%}
+
 tau=(M_star-cos(phi_star))/(omega_c_star*sin(phi_star));
 alpha_tau=(cos(phi_star)-1/M_star)/(omega_c_star*sin(phi_star));
 
 R_d=(1+tau*s)/(1+alpha_tau*s);
-figure(127);
+figure(5);
 bode(R_d);
 title("Rete Anticipatrice");
 %%
-gain=1.5;
+gain=1.95;
 
 LL = GGe * R_d * gain;
 LL_Ei_min = GGe_Ei_min * R_d * gain;
 LL_Ei_max = GGe_Ei_max * R_d * gain;
 
-figure(129);
-title("risultato finale");
+figure(6);
 
 [Mag,phase,omega] = bode(LL, {omega_range_min, omega_range_max});
 [Mag_min,phase_min,omega_min] = bode(LL_Ei_min, {omega_range_min, omega_range_max});
 [Mag_max,phase_max,omega_max] = bode(LL_Ei_max, {omega_range_min, omega_range_max});
 
 
-patch([omega_range_min, omega_c_min, omega_c_min, omega_range_min], [0, 0, -200, -200], 'r', 'FaceAlpha', 0.3); grid on
-patch([omega_n, omega_range_max, omega_range_max, omega_n], [Bn, Bn, 200, 200], 'r', 'FaceAlpha', 0.3); grid on
+patch([omega_range_min, omega_c_min, omega_c_min, omega_range_min], [0, 0, -200, -200], 'r', 'FaceAlpha', 0.2); grid on
+patch([omega_n, omega_range_max, omega_range_max, omega_n], [Bn, Bn, 200, 200], 'r', 'FaceAlpha', 0.4); grid on
 hold on;
 
-Legend_dB=["B_n";
-    "\omega_{c,min}"
-    "LL(j\omega)" ];
+Legend_dB=["Att. dist. uscita";
+    "Att. dist. misura"
+     ];
+ 
 legend(Legend_dB);
 
 margin(Mag,phase,omega);grid on;
-bode(LL, 'b', LL_Ei_min, 'm', LL_Ei_max, 'k');
+bode( LL_Ei_min, 'm', LL_Ei_max, 'k');
 
 patch([omega_c_min, omega_n, omega_n, omega_c_min], [-200, -200, phase_L_jwc_min, phase_L_jwc_min], 'r', 'FaceAlpha', 0.3); grid on
-Legend_arg=["LL(j\omega)";"M_{f,min}"] ;
+Legend_arg=["L(j\omega)";
+    "L_{Ei min}";
+    "L_{Eimax}";
+    "M_{f,min}"] ;
 legend(Legend_arg);
 
+title("L con le variazioni su Ei");
 hold off;
 
 %% Graphical constraints of the step response
-figure(52); 
+figure(7); 
 
+step(W*LL/(1+LL),T_simulation,'b')
+hold on
+% add overshoot constraint
+patch([0,T_simulation,T_simulation,0],[W*(1+S_100),W*(1+S_100),W+1,W+1],'red','FaceAlpha',0.3);
+hold on;
+ylim([0,W+1]);
+
+
+% add Settling time constraint
+
+patch([Ta_1,T_simulation,T_simulation,Ta_1],[W*(1+0.01),W*(1+0.01),W+1,W+1], 'green','FaceAlpha',0.2);
+
+patch([Ta_1,T_simulation,T_simulation,Ta_1],[W*(1-0.01),W*(1-0.01),0,0], 'green','FaceAlpha', 0.2);
+Legend=["L(s) Step Response";"Overshoot Constraint"; "Settling time Cons"];
+legend(Legend);
+title("Risposta di L(s) a ingresso a gradino");
+hold off;
+
+%% Graphical constraints of the step response
+figure(8); 
+
+step(W*LL/(1+LL),T_simulation,'b')
+step(W*LL_Ei_min/(1+LL_Ei_min),T_simulation,'b')
 step(W*LL_Ei_max/(1+LL_Ei_max),T_simulation,'b')
 hold on
 % add overshoot constraint
@@ -211,9 +238,10 @@ patch([Ta_1,T_simulation,T_simulation,Ta_1],[W*(1+0.01),W*(1+0.01),W+1,W+1], 'gr
 
 patch([Ta_1,T_simulation,T_simulation,Ta_1],[W*(1-0.01),W*(1-0.01),0,0], 'green','FaceAlpha', 0.2);
 
-hold off
 Legend=["L(s) Step Response";"Overshoot Constraint"; "Settling time Cons"];
 legend(Legend);
+title("Risposta di L(s) a ingresso a gradino");
+hold off;
 
 %% Simulink
 clc
@@ -225,7 +253,6 @@ R=(R_d*gain)/s;
 num_r=n_r{1};
 den_r=d_r{1};
 
-%x0=[0;0];
 teta_e=0; %non modifica il sistema
 x0=[0,We];
 u_e=1110.7222;
